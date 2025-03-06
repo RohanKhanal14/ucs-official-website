5d5834a42ebfc4b05f275ef51b364e13f0f6cb4c
Rohan <rohankhanal114@gmail.com>
2025-03-06 21:33:24 +0545

    vercel insites

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -31,6 +31,7 @@ export default function RootLayout({
       >
         <main className="min-h-screen">
           {children}
+          <SpeedInsights/>
           <Toaster />
         </main>
       </body>
